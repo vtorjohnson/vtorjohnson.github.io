7a4e821bb4023c5e7584c5f9935b76a4f527cddd
Tor <vtorjohnson@icloud.com>
2023-12-03 21:57:23 -0500

    fix: update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/github-pages',
+  
+}
+
 
 module.exports = nextConfig
