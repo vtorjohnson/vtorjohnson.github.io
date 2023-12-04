f2cad565065c36c3dc484f3d330548fc1098bf9a
Tor <vtorjohnson@icloud.com>
2023-12-03 22:16:23 -0500

    fix: add back basePath

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 
 const nextConfig = {
     output: 'export',
-//     reactStrictMode: true,
+    reactStrictMode: true,
   
-//    // Add basePath
-//     basePath: '/github-pages',
+   // Add basePath
+    basePath: '/github-pages',
+    images: {
+        loader: 'custom',
+        loaderFile: './my-loader.ts',
+    },
   
 }
 
