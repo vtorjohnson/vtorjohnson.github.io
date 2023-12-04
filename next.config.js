a5002cfcc0a1cd62dd0bdfeaa4ca2c98e30bac8f
Tor <vtorjohnson@icloud.com>
2023-12-03 22:17:55 -0500

    fix: remove images from nextConfig

--- next.config.js
+++ next.config.js
@@ -4,13 +4,7 @@ const nextConfig = {
     output: 'export',
     reactStrictMode: true,
   
-   // Add basePath
     basePath: '/github-pages',
-    images: {
-        loader: 'custom',
-        loaderFile: './my-loader.ts',
-    },
-  
 }
 
 
